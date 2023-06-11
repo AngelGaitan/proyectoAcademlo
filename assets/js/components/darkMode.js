dda37727b29275ca3bd662fa6db82a3e0a720d3c
Alexis Arango <134032888+AlexisArangoo@users.noreply.github.com>
2023-06-11 00:14:42 -0500

    edwef

--- assets/js/components/darkMode.js
+++ assets/js/components/darkMode.js
@@ -1,29 +1,20 @@
-// ahora vamos a hacer el dark mode 
-
-
+// ahora vamos a hacer el dark mode
 
 export let darkMode = function () {
+
+let darktheme = document.querySelector("#dark");
+
+   darktheme.addEventListener("click", () => {
+    
+     document.body.classList.toggle("dark_theme");
      
-    let darktheme = document.querySelector('#dark')
-   
-    darktheme.addEventListener('click', () => {
-       
-        document.body.classList.toggle("dark_theme")
-        
-        if(document.body.classList.contains("dark_theme")){
-         darktheme.innerHTML = "<i class='bx bxs-sun' ></i>"
-        }else{
-            darktheme.innerHTML = "<i class='bx bxs-moon'></i>"   
-        }
-    })
-}
-darkMode()
+     if (document.body.classList.contains("dark_theme")) {
+       darktheme.innerHTML = "<i class='bx bxs-sun' ></i>";
+     } else {
+       darktheme.innerHTML = "<i class='bx bxs-moon'></i>";
+     }
+   });
 
-
-
-
-let rotation = document.querySelector('.rot')
-
-rotation.addEventListener('click', () => {
-    document.classList.contains('rot:hover')
-})
+  
+};
+darkMode();
